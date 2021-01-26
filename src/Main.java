a831e68634d923a33070347ce17f94f748e34186
Tyler Beringer <99tylerberinger@gmail.com>
2021-01-26 16:04:13 -0500

    Removed unused imports

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Border;
-import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
 import javafx.stage.Stage;
-
-import java.awt.*;
 
 public class Main extends Application {
 
