eb11452f330809e7d4f7a0868e0f2a50d748ea08
Tyler Beringer <99tylerberinger@gmail.com>
2021-01-28 16:16:36 -0500

    Added generatePassword()

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,19 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.util.Random;
+
 public class Main extends Application {
 
-    private static Button button;
-    private static Label label;
+    private static Button button = new Button("Generate Password");
+    private static Label label = new Label("...");
+
+    private final String pLettersLower = "abcdefghijklmnopqrstuvwxyz";
+    private final String pLettersUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private final String pNumbers = "0123456789";
+    private final String pSpecialCharacters = "-=[];,./~!@#$%^&*()_+{}:<>?";
+
+    private final Random random = new Random();
 
     public static void main(String[] args) {
         launch(args);
@@ -19,9 +28,7 @@ public class Main extends Application {
 
         primaryStage.setTitle("password-generator");
 
-        button = new Button("Generate Password");
-
-        label = new Label("aaa0aaa#aaa0");
+        button.setOnMouseClicked(e -> generatePassword());
 
         // Set layout
         final VBox layout = new VBox();
@@ -34,6 +41,40 @@ public class Main extends Application {
         // Display it
         primaryStage.setScene(scene);
         primaryStage.show();
+
+    }
+
+    private void generatePassword() {
+
+        // Default: AAA0aaa-AAA-
+
+        StringBuilder stringBuilder = new StringBuilder(12);
+
+        for (int i = 0; i < stringBuilder.capacity(); i+=4) {
+            for (int ii = 0; ii < 4; ii++) {
+
+                if (ii == 3) {
+
+                    if (i == 0) {
+                        stringBuilder.append(pNumbers.charAt(random.nextInt(pNumbers.length())));
+                    } else {
+                        stringBuilder.append(pSpecialCharacters.charAt(random.nextInt(pSpecialCharacters.length())));
+                    }
+
+                } else {
+
+                    if (i == 4) {
+                        stringBuilder.append(pLettersLower.charAt(random.nextInt(pLettersLower.length())));
+                    } else {
+                        stringBuilder.append(pLettersUpper.charAt(random.nextInt(pLettersUpper.length())));
+                    }
+
+                }
+
+            }
+        }
+
+        label.setText(stringBuilder.toString());
 
     }
 
