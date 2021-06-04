f1dcb6d49688fedd31bede64a15b5066a78b9389
Tyler Beringer <99tylerberinger@gmail.com>
2021-06-03 22:59:07 -0400

    Added more widgets for customizability, deleted Theme.css

--- PasswordGenerator/src/com/gmail/__99tylerberinger/passwordgenerator/Main.java
+++ PasswordGenerator/src/com/gmail/__99tylerberinger/passwordgenerator/Main.java
@@ -5,6 +5,7 @@ import javafx.beans.property.SimpleStringProperty;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.Spinner;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,15 +13,12 @@ import java.util.Random;
 
 public class Main extends Application {
 
-    final SimpleStringProperty password = new SimpleStringProperty("✖✖✖✖✖✖✖✖✖✖✖✖");
-
-    final int amountOfChunks = 3;
-    final int charsPerChunk = 4;
+    final SimpleStringProperty password = new SimpleStringProperty("✖✖✖✖✖✖✖✖");
 
     private final String[] leadingChars = {"abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ", "0123456789"};
-    private final String trailingChars = "!@#$%^&*()-=_+";
+    private String currentLeadingChars = leadingChars[0];
 
-    private String currentLeadingCharsSection = leadingChars[0];
+    private final String trailingChars = "!@#$%^&*()-=_+";
 
     public static void main(String[] args) {
         launch(args);
@@ -30,15 +28,21 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
 
         // Widgets
-        final Button button = new Button("Generate Password");
-        button.setOnMouseClicked(e -> generatePassword());
+        final Label labelPassword = new Label();
+        labelPassword.textProperty().bind(password);
 
-        final Label label = new Label();
-        label.textProperty().bind(password);
+        final Label labelAmountOfChunks = new Label("amount of chunks:");
+        final Spinner<Integer> spinnerAmountOfChunks = new Spinner<>(2, 10, 3, 1);
+
+        final Label labelCharsPerChunk = new Label("chars per chunk:");
+        final Spinner<Integer> spinnerCharsPerChunk = new Spinner<>(3, 10, 4, 1);
+
+        final Button buttonGenerate = new Button("GENERATE");
+        buttonGenerate.setOnMouseClicked(e -> generate(spinnerAmountOfChunks.getValue(), spinnerCharsPerChunk.getValue()));
 
         // Scene
-        final Scene scene = new Scene(new VBox(button, label));
-        scene.getStylesheets().add("Theme.css");
+        final Scene scene = new Scene(new VBox(labelPassword, labelAmountOfChunks, spinnerAmountOfChunks, labelCharsPerChunk, spinnerCharsPerChunk, buttonGenerate));
+        // TODO: scene.getStylesheets().add("Theme.css");
 
         // Stage
         primaryStage.setTitle("password-generator");
@@ -48,27 +52,26 @@ public class Main extends Application {
 
     }
 
-    private void generatePassword() {
+    private void generate(int amountOfChunks, int charsPerChunk) {
 
         final StringBuilder stringBuilder = new StringBuilder(amountOfChunks * charsPerChunk);
-
         final Random random = new Random();
 
         for (int chunk = 0; chunk < amountOfChunks; chunk++) {
 
             // Get a new leadingChars section for the chunk
-            String newLeadingCharsSection = currentLeadingCharsSection;
-            while (newLeadingCharsSection == currentLeadingCharsSection) {
-                newLeadingCharsSection = leadingChars[random.nextInt(leadingChars.length)];
+            String newCurrentLeadingChars = currentLeadingChars;
+            while (newCurrentLeadingChars == currentLeadingChars) {
+                newCurrentLeadingChars = leadingChars[random.nextInt(leadingChars.length)];
             }
-            currentLeadingCharsSection = newLeadingCharsSection;
+            currentLeadingChars = newCurrentLeadingChars;
 
             // Fill the chunk with the leadingChars and trailingChar
             for (int i = 0; i < charsPerChunk; i++) {
 
                 if (i < (charsPerChunk - 1)) {
 
-                    final char leadingChar = currentLeadingCharsSection.charAt(random.nextInt(currentLeadingCharsSection.length()));
+                    final char leadingChar = currentLeadingChars.charAt(random.nextInt(currentLeadingChars.length()));
                     stringBuilder.append(leadingChar);
 
                 } else {
